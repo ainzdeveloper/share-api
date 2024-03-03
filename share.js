49194cf8cb7fd67bd9d1dcbc525b3a8a74dbbbca
ainzdeveloper <158009579+ainzdeveloper@users.noreply.github.com>
2024-03-03 15:05:26 +0800

    Update share.js

--- share.js
+++ share.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const crypto = require('crypto');
-
+const cheerio = require('cheerio');
+app.use(express.static('public'));
 async function qt(page, search) {
   try {
     
