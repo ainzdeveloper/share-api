e91eaf2c0563ad50eafd708bc907879048caa695
ainzdeveloper <158009579+ainzdeveloper@users.noreply.github.com>
2024-03-13 22:13:58 +0800

    Update share.js

--- share.js
+++ share.js
@@ -23,14 +23,7 @@ const deleteAfter = 60 * 60;
 
 let sharedCount = 0;
 let timer = null;
-try {
-      const a = await axios.get(`https://graph.facebook.com/me?access_token=${token}`);
-      if (a.data.error) {
-        return res.status(401).json({ error: 'Invalid access token' });
-      }
-    } catch (error) {
-      return res.status(401).json({ error: 'Invalid access token' });
-    }
+
     
 async function sharePost() {
   try {
